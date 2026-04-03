bb340bcafa1e880853fa1d5441f69a5521d86441
lchamard <lchamard@proton.me>
2026-04-03 09:42:27 +0200

    [install script] little change on install script

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# Move to home
-cd ~
-mkdir -p .local
-mkdir -p .local/bin
+mkdir -p ~/.local
+mkdir -p ~/.local/bin
 # Remove old .marvin
-rm -Rf .marvin
+rm -Rf ~/.marvin
 # Clone the repo
-git clone https://www.github.com/SeigneurLefou/Marvin-the-Paranoid-Assistant .marvin
+git clone https://www.github.com/SeigneurLefou/Marvin-the-Paranoid-Assistant ~/.marvin
 # Move to .marvin
-cd .marvin
+make -C ~/.marvin build
 
 # Add .local/bin to the PATH
 export PATH=$PATH:$HOME/.local/bin/
@@ -17,4 +15,3 @@ echo "export PATH=$PATH:$HOME/.local/bin/" >> ~/.zshrc
 # For security add previous line in the config file
 echo "export PATH=$PATH:$HOME/.local/bin/" >> ~/.bashrc
 # Build the binary
-make build
